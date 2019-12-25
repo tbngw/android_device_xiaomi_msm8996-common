d1c89f14af1211fce35be55b622214bef32436a3
Han Wang <416810799@qq.com>
2019-12-25 17:08:32 +0100

    msm8996-common: Disable interaction boost
     * Does not work correctly with stock perf stack, switching
       profiles do not have any real effect at all.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,7 +165,6 @@ TARGET_USES_MKE2FS := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-TARGET_USES_INTERACTION_BOOST := true
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
