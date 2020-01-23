605d7161fa35f88e4e9a3fe71c5843df0b331ebf
Jonas Cardoso <jonascard60@gmail.com>
2020-01-23 20:40:45 -0300

    msm8996-common: Update CarrierConfig from LA.UM.8.6.r1-02900-89xx.0

--- msm8996.mk
+++ msm8996.mk
@@ -183,6 +183,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# CarrierConfig
+PRODUCT_PACKAGES += \
+    CarrierConfig
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
